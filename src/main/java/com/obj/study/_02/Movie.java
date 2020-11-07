8905c9b5c1b7df8740dad28d9168226e9dcb0a22
preciouStar <seongjin605@users.noreply.github.com>
2020-11-07 22:39:02 +0900

    Update comment

--- src/main/java/com/obj/study/_02/Movie.java
+++ src/main/java/com/obj/study/_02/Movie.java
@@ -28,9 +28,12 @@ public class Movie {
     }
 
     public Money calculateMovieFee(Screening screening) {
+        /*
+        책임의 위치를 결정하기 위해 조건문을 사용하는 것은 협력의 설계 측면에서 좋지 못함.
+        예외 케이스를 간소화하고 일관성을 유지할 수 있는 방법으로 NoneDiscountPolicy 클래스 추가
         if(discountPolicy == null) {
             return fee;
-        }
+        }*/
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
 }
