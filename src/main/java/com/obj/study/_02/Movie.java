6ae01a87e9d48fb93a9257aa7792f396244a6495
preciouStar <seongjin605@users.noreply.github.com>
2020-11-13 22:28:37 +0900

    접근지정자 변경

--- src/main/java/com/obj/study/_02/Movie.java
+++ src/main/java/com/obj/study/_02/Movie.java
@@ -12,22 +12,22 @@ public class Movie {
     private Money fee;
     private DiscountPolicy discountPolicy;
 
-    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+    protected Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
         this.title = title;
         this.runningTime = runningTime;
         this.fee = fee;
         this.discountPolicy = discountPolicy;
     }
 
-    public String getTitle() {
+    protected String getTitle() {
         return this.title;
     }
 
-    public Money getFee() {
+    protected Money getFee() {
         return fee;
     }
 
-    public Money calculateMovieFee(Screening screening) {
+    protected Money calculateMovieFee(Screening screening) {
         /*
         책임의 위치를 결정하기 위해 조건문을 사용하는 것은 협력의 설계 측면에서 좋지 못함.
         예외 케이스를 간소화하고 일관성을 유지할 수 있는 방법으로 NoneDiscountPolicy 클래스 추가
